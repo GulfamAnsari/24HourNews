338b5607ad74ce344c0432fd65d7ba82f230bb9b
sarthak subramanian <sarthak@crindo.com>
2017-04-23 15:45:27 +0530

    add footer

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 	<link rel="stylesheet" href="style.css">
 	<!--Icons -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	 
-   <link href='rangeSlider.css' rel='stylesheet'> 
 	<!--Non JavaScript Tag-->
 	<noscript>
 		JavaScript is Not Enable.
@@ -18,95 +16,14 @@
 		Here are the <a href="http://www.enable-javascript.com/" target="_blank">
 		instructions how to enable JavaScript in your web browser</a>.
 	</noscript>
-	<style>
-		.heading
-		{
-		font-family:Comic Sans MS;
-		font-size:20px;
-		font-weight:bold;
-		}
-		.para
-		{
-		font-family:Euphemia;
-		font-size:15px;
-		}
-		.navHeading
-		{
-		font-size:16px;
-		font-family:Microsoft JhengHei;
-		text-transform: uppercase;
-		line-height:2px;
-		word-spacing:1px;
-		}
-		.navbar-custom
-		{
-		position:relative;
-		background-color:#378DFF;
-		}
-		.navbar-custom #smallMenu ul li a
-		{
-		color:white;
-		}
-		.navbar-custom #smallMenu ul li a:hover
-		{
-		background-color:#D6EFED;
-		color:#535857;
-		}
-		
-		
-		
-		.header
-		{
-			padding:20px 0px 20px 0px;
-			background-color:white;
-		}
-		
-		.header .header-search #s
-		{
-			border:none;
-			background: #EFEBE7;
-			width:80%;
-			padding:10px;
-			
-		}
-		.header .sbutton
-		{
-			border:none;
-			background-color: #f47555;
-			color:white;
-			width: 40px;
-			height: 40px;
-			font-size:20px;
-			
-		}
-		.header .social
-		{
-			text-align:right;
-			margin-top:20px;
-			
-		}
-		.header .social ul li a
-		{
-			color:white;
-			padding:15px;
-			
-		}
-		@media (max-width: 768px) 
-		{
-		.header .col-sm-2,.header .col-sm-4
-		{
-			text-align:center;
-		}
-		}
-		
-	</style>
+
 </head>
 <body style="background-color: #f0ede9;">
 <!--Header--->
 <div class="container header">
 	<div>
 		<div class="col-sm-2">
-			<a><img src="http://www.news24nepal.tv/wp-content/themes/news24desktop/images/logo.png" height="50px" style="margin-top:-15px"></img></a>
+			<a href="/24HourNews/"><img src="http://www.news24nepal.tv/wp-content/themes/news24desktop/images/logo.png" height="50px" style="margin-top:-15px"></img></a>
 		</div>
 		<div class="col-sm-6 header-search text-center" >
 			<form method="get" id="searchform" class="search-form" >
@@ -141,7 +58,7 @@
 		<div class="collapse navbar-collapse" id="smallMenu" >
 			<ul class="nav navbar-nav navbar-left">
 				<li><a  class="navHeading" href="#"><i class="fa fa-home" style="padding-right:5px"></i>Top Stories</a></li>
-				<li><a  class="navHeading" href="#">World</a></li>
+				<li><a  class="navHeading" href="WORLD/">world</a></li>
 				<li><a  class="navHeading" href="#">India</a></li>
 				<li><a  class="navHeading" href="#">City</a></li>
 				<li><a  class="navHeading" href="#">Technology</a></li>
@@ -153,6 +70,35 @@
 		</div>
 	</div>
 </div>
+
+<!----------Footer---------- -->
+<div class="footer " >
+	<div class="container">
+		<div class="col-sm-4">
+			<p class="para">CONTACT US</p>
+			<p class="para">CALL <span >+91-000-0000</span></p>
+			<p class="para">EMAIL<span>abcxyz@gmail.com</span></p>
+		</div>
+		<div class=" col-sm-4 ">
+				<p class="para" style="color:white;text-align:center;" >FOLLOW US</p>
+				<div class="social ">
+					<ul class="list-inline text-center" >
+						<li><a href="#" style="background: #375593"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" style="background: #0eb6f6"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" style="background: #dd4b39"><i class="fa fa-youtube"></i></a></li>
+						<li><a href="#" style="background: #e32c26"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+				
+		</div>
+		<div class="col-sm-4">
+			<p class="para"  >NEWSLETTER</p>
+			<p class="para" >Subscribe  to our newslater to receive news and updates from our websites</p>
+			<input type="text" name="subscribe"  placeholder="Enter Email" />
+			<button type="button" class="btn btn-success">Subscribe</button>
+		</div>
+	</div>
+</div >
 
 	
 		<!--------------------------------
